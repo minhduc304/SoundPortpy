3fe32f5330dfbc90f1405237ff8ad5284e0e3fac
minhduc304 <mihducv42@gmail.com>
2025-03-25 09:07:46 +0700

    Fixed connection issues

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import os 
 from dotenv import load_dotenv
 
@@ -8,13 +8,18 @@ from dotenv import load_dotenv
 load_dotenv()
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
 # Authenticate with Spotify
-auth_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
-sp = spotipy.Spotify(auth_manager=auth_manager)
+scope = "playlist-read-private"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                                               client_secret=SPOTIFY_CLIENT_SECRET,
+                                               redirect_uri=SPOTIFY_REDIRECT_URI,
+                                               scope=scope))
+
 
 # Get the user's playlists
-playlists = sp.user_playlists('spotify')
+playlists = sp.user_playlist(user='spotify')
 while playlists:
     for i, playlist in enumerate(playlists['items']):
         print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
@@ -22,3 +27,6 @@ while playlists:
         playlists = sp.next(playlists)
     else:
         playlists = None
+
+
+
