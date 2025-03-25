7de2e54e2d2c75cf0e9079f070ab49210325f97d
minhduc304 <mihducv42@gmail.com>
2025-03-25 13:25:57 +0700

    Testing CLI library to output tracks from playlist

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os 
 from dotenv import load_dotenv
+from collections import defaultdict
+import click
 
 
 # Load environment variables
@@ -19,21 +21,38 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
 
 
 # Get current user
-current_user = sp.me()["uri"].split(":")[-1]
+current_user = sp.me()['uri'].split(":")[-1]
 
 
 # Get the user's playlists
+playlist_URIs =[]
 playlists = sp.user_playlists(user=current_user)
 while playlists:
     for i, playlist in enumerate(playlists['items']):
-        print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
+        # print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
+        playlist_URIs.append(playlist['uri'].split(':')[2] + ":" + playlist['name'])
     if playlists['next']:
         playlists = sp.next(playlists)
     else:
         playlists = None
 
-print(playlists)
+
+tracks = defaultdict(list)
+
+for uri in playlist_URIs:
+    for track in sp.playlist_tracks(uri.split(":")[0])['items']:
+        tracks[uri.split(":")[-1]].append(track['track']['name'])
 
 
 
+@click.command()
+@click.option('--name', prompt='Enter the name of the playlist you want to see the tracks of', help='The name of the playlist')
+
+def get_tracks(name):
+    click.echo(f"Tracks in {name}:")
+    for track in tracks[name]:
+        click.echo(track)
+
+if __name__ == '__main__':
+    get_tracks()
 
