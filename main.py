f3796562741f13a0ae6dea15edc24f5107e24422
minhduc304 <mihducv42@gmail.com>
2025-03-26 20:36:13 +0700

    Changed main cli function, changed user prompt format

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from collections import defaultdict
 import click
+from tqdm import tqdm
 
 
 # Load environment variables
@@ -15,9 +16,9 @@ SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 # Authenticate with Spotify
 scope = "playlist-read-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
-                                               client_secret=SPOTIFY_CLIENT_SECRET,
-                                               redirect_uri=SPOTIFY_REDIRECT_URI,
-                                               scope=scope))
+                                            client_secret=SPOTIFY_CLIENT_SECRET,
+                                            redirect_uri=SPOTIFY_REDIRECT_URI,
+                                            scope=scope))
 
 # sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID,
 #                                                            client_secret=SPOTIFY_CLIENT_SECRET))
@@ -42,50 +43,49 @@ while playlists:
 
 tracks = defaultdict(list)
 
-for uri in playlist_URIs:
+for uri in tqdm(playlist_URIs):
     for track in sp.playlist_tracks(uri.split(":")[0])['items']:
         tracks[uri.split(":")[-1]].append(track['track']['name'])
 
-@click.command()
-# @click.option('i', '--input', help='Input to the CLI')
+
+@click.group()
 def cli():
-    click.echo("Welcome to the Spotable CLI". center(50, "-"))
-    click.echo("What would you like to do:")
-
-    try:
-        click.echo("[1] Get all playlists of the current user")
-        click.echo("[2] Get tracks of a playlist")
-
-        choice = click.prompt("Enter your your choice: ", type=int)
+    pass
     
-        if choice == 1:
-            get_playlists()
-        elif choice == 2:
-            get_tracks()
-        
-        cli()
-    except:
-        click.echo("Invalid choice. Please try again.")
 
-
-@click.command()
+@cli.command()
 def get_playlists():
     """Get the playlists of the current user"""
-    click.echo("Playlists:")
+    click.echo("Here are your playlists:")
     for playlist in playlist_URIs:
         click.echo(playlist.split(":")[-1])
 
 
-@click.command()
+@cli.command()
 @click.option('--name', prompt='Enter the name of the playlist you want to see the tracks of', help='The name of the playlist')
 def get_tracks(name):
     """Get the tracks of a playlist. Make sure to copy and paste the URL that you were redirected to after running the app."""
-    click.echo(f"Tracks in {name}:")
+    click.echo(f"Tracks from {name}:")
     for track in tracks[name]:
         click.echo(track)
 
+def main():
+    click.echo("Welcome to the Spotable CLI". center(50, "-"))
 
-
+    value = click.prompt('What would you like to do: ', type=click.Choice(list(cli.commands.keys()) + ['exit']))
+    click.echo('\n')
+    click.echo("get_playlists: Get all playlists of the current user")
+    click.echo("get_tracks: Get tracks of a playlist")
+    while value != 'exit':  
+        cli.commands[value]()
+    
+        if value == 'get-playlists':
+            get_playlists()
+            
+        elif value == 'get-tracks':
+            get_tracks()
+            
+        
 
 if __name__ == '__main__':
-    cli()
+    main()
