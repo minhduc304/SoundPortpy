f6e9ac1596511a2e039d96864c7203dd3b4f33dd
minhduc304 <mihducv42@gmail.com>
2025-03-31 09:15:08 +0700

    Fixed some issues with command input, added more command descriptions and comments

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import spotipy 
-from spotipy.oauth2 import SpotifyOAuth 
-import os 
-from dotenv import load_dotenv 
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
 from collections import defaultdict
-import click 
-from tqdm import tqdm 
+import click
+from tqdm import tqdm
 from utils.capture_redirect import get_spotify_auth_code
-import time 
-
+import time
 
 
 def setup_auth():
-    """Authentication setup to capture redirect URL and get token info"""
+    """
+    Authentication setup to capture redirect URL and get token info.
+    Sets up the Spotify client with the user's credentials.
+
+    Returns:
+        list: A list containing the Spotify client and the current user's ID.
+    """
 
     # Load environment variables
     scope = "playlist-read-private"
     from dotenv import load_dotenv
+
     load_dotenv()
 
     client_id = os.getenv("SPOTIFY_CLIENT_ID")
     client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")    
+    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
-    # Get token info automatically
+    # # Get token info automatically
     # token_info = get_spotify_auth_code(client_id, client_secret, redirect_uri, scope)
 
     # if token_info is None:
@@ -33,65 +39,99 @@ def setup_auth():
         client_id=client_id,
         client_secret=client_secret,
         redirect_uri=redirect_uri,
-        scope=scope
+        scope=scope,
     )
 
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
     # Get current user
-    current_user = sp.me()['uri'].split(":")[-1]
-    
-    return[sp, current_user]
+    current_user = sp.me()["uri"].split(":")[-1]
+
+    return [sp, current_user]
+
 
 def setup_app(auth):
+    """
+    Sets up the application by retrieving the user's playlists and tracks.
+
+    Args:
+        auth (list): A list containing the Spotify client and the current user's ID.
+
+    Returns:
+        list: A list containing the playlist URIs and the tracks.
+    """
     # Get the user's playlists
     sp = auth[0]
     current_user = auth[1]
-    playlist_URIs =[]
+    playlist_URIs = []
     playlists = sp.user_playlists(user=current_user)
     while playlists:
-        for i, playlist in enumerate(playlists['items']):
+        for playlist in playlists["items"]:
             # print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
-            playlist_URIs.append(playlist['uri'].split(':')[2] + ":" + playlist['name'])
-        if playlists['next']:
+            playlist_URIs.append(playlist["uri"].split(":")[2] + ":" + playlist["name"])
+        if playlists["next"]:
             playlists = sp.next(playlists)
         else:
             playlists = None
 
-
     tracks = defaultdict(list)
 
     for uri in tqdm(playlist_URIs):
-        for track in sp.playlist_tracks(uri.split(":")[0])['items']:
-            tracks[uri.split(":")[-1]].append(track['track']['name'])
+        try:
+            for track in sp.playlist_tracks(uri.split(":")[0])["items"]:
+                tracks[uri.split(":")[-1]].append(track["track"]["name"])
+        except Exception as e:
+            print(f"Error getting tracks for playlist {uri}: {e}")
 
-    return playlist_URIs, tracks
+    return [playlist_URIs, tracks]
 
 
 @click.group()
 def cli():
+    """
+    Defines the command-line interface for the application.
+    """
     pass
-    
+
 
 @cli.command()
-def get_playlists(playlist_URIs):
-    """Get the playlists of the current user"""
+def get_playlists():
+    """
+    Get the playlists of the current user.
+    """
     click.echo("Here are your playlists:")
-    for playlist in playlist_URIs:
-        click.echo(playlist.split(":")[-1])
+    # Access playlist_URIs from the main function's scope
+    try:
+        for playlist in globals()['playlist_URIs']:
+            click.echo(playlist.split(":")[-1])
+    except NameError:
+        click.echo("Error: playlist_URIs not found. Please run the app first.")
+
 
 
 @cli.command()
-@click.option('--name', prompt='Enter the name of the playlist you want to see the tracks of', help='The name of the playlist')
-def get_tracks(tracks, name):
-    """Get the tracks of a playlist. Make sure to copy and paste the URL that you were redirected to after running the app."""
+@click.option(
+    "--name",
+    prompt="Enter the name of the playlist you want to see the tracks of",
+    help="The name of the playlist",
+)
+def get_tracks(name, tracks):
+    """
+    Get the tracks of a playlist.
+    Make sure to copy and paste the URL that you were redirected to after running the app.
+    """
     click.echo(f"Tracks from {name}:")
     for track in tracks[name]:
         click.echo(track)
 
-def main():
 
-    click.echo("""
+def main():
+    """
+    Main function of the application.
+    """
+
+    click.echo(
+        """
                                                                                                                         
                                                           ,-.----.                                  
   .--.--.                                                 \    /  \                         ___     
@@ -108,7 +148,8 @@ def main():
                        `--`----'   '---'        \   \  /  `---'.|                          ---`-'   
                                                  `----'     `---`                                   
                                                                                                     
-           
+
+
     Welcome to the SoundPort CLI
     If this is your first time using this app please reference the README 
     in order to know how to obtain your Spotify API authentication. You will be redirected to a webpage after to authenticate with Spotify.
@@ -118,24 +159,31 @@ def main():
     get_playlists: Get all playlists of the current user
     get_tracks:    Get tracks of a playlist
     exit:
-    """)
-    
-    time.sleep(10)
-    # Setup authentication and app
-    playlist_URIs, tracks = setup_app(setup_auth())
+    """
+    )
 
-    value = click.prompt('What would you like to do: ', type=click.Choice(list(cli.commands.keys()) + ['exit']))
-    click.echo('\n')
-    
-    while value != 'exit':  
-        cli.commands[value]()
-    
-        if value == 'get-playlists':
-            get_playlists(playlist_URIs)
-            
-        elif value == 'get-tracks':
-            get_tracks(tracks)
-            
-        
-if __name__ == '__main__':
+    # time.sleep(5)
+    # Setup authentication and app
+    input_list = setup_app(setup_auth())
+    playlist_URIs = input_list[0]
+    tracks = input_list[1]
+
+    valid_choices = list(cli.commands.keys()) + ["exit"]
+    value = click.prompt(
+        "What would you like to do: ", type=click.Choice(valid_choices)
+    )
+    click.echo("\n")
+
+    while value != "exit":
+        if value == "get-playlists":
+            get_playlists()
+        elif value == "get-tracks":
+            get_tracks(name=value, tracks=tracks)
+        value = click.prompt(
+            "What would you like to do: ", type=click.Choice(valid_choices)
+        )
+        click.echo("\n")
+
+
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -5,7 +5,18 @@ setup(
     version='0.1.0',
     py_modules=['main'],
     install_requires=[
-        'Click',
+        'certifi==2025.1.31',
+        'charset-normalizer==3.4.1',
+        'click==8.1.8',
+        'dotenv==0.9.9',
+        'idna==3.10',
+        'python-dotenv==1.0.1',
+        'redis==5.2.1',
+        'requests==2.32.3',
+        'setuptools==78.1.0',
+        'spotipy==2.25.1',
+        'tqdm==4.67.1',
+        'urllib3==2.3.0',
     ],
     entry_points={
         'console_scripts': [
@@ -13,4 +24,3 @@ setup(
         ],
     },
 )
-
